3432b99ad0140928c03f22eb7b0c6af948b2e9b9
Carlos Liu <Carlos-Liu@users.noreply.github.com>
2021-01-08 17:47:27 +0800

    Add Status file prototype.

---
+++ StatusFilePrototype/StatusFilePrototype/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StatusFilePrototype
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void DoWorkForSameCode()
+    {
+      StatusFileUtil.AddEvent("Thread", 1001, "Dummy error.", DateTime.Now);
+    }
+    
+    private int GetRandom(int min, int max)
+    {
+      var random = new Random((int)DateTime.Now.Ticks);
+      return random.Next(min, max);
+    }
+
+    private async Task WorkToWriteSameErrorCodeAsync()
+    {
+      int threadsCount = int.Parse(ThreadsCountTextBox.Text.Trim());
+      for (int i = 0; i < threadsCount; ++i)
+      {
+        var newThread = new Thread(DoWorkForSameCode);
+        newThread.Start();
+        LogListBox.Items.Add($"#{i}\t{DateTime.Now} New thread is started.");
+
+        await Task.Delay(GetRandom(100, 1000)).ConfigureAwait(true);
+      }
+    }
+
+    private void SameCodeButton_Click(object sender, RoutedEventArgs e)
+    {
+      WorkToWriteSameErrorCodeAsync();
+    }
+
+
+    private void DoWorkForDiffCode()
+    {
+      var randomErrorCode = GetRandom(1001, 1006);
+      StatusFileUtil.AddEvent("Thread", randomErrorCode, "Dummy error.", DateTime.Now);
+    }
+
+    private async Task WorkToWriteDiffErrorCodeAsync()
+    {
+      int threadsCount = int.Parse(ThreadsCountTextBox.Text.Trim());
+      for (int i = 0; i < threadsCount; ++i)
+      {
+        var newThread = new Thread(DoWorkForDiffCode);
+        newThread.Start();
+        LogListBox.Items.Add($"#{i}\t{DateTime.Now} New thread is started.");
+
+        await Task.Delay(GetRandom(100, 1000)).ConfigureAwait(true);
+      }
+    }
+
+    private void DiffCodeButton_Click(object sender, RoutedEventArgs e)
+    {
+      WorkToWriteDiffErrorCodeAsync();
+    }
+  }
+}
---
+++ StatusFilePrototype/StatusFilePrototype/StatusFileUtil.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Threading;
+
+namespace StatusFilePrototype
+{
+  public static class StatusFileUtil
+  {
+    private const string LogFilename = "beclsa.connect.events.log";
+    private static readonly string LogFileFullPath = GetLogFileWithFullPath();
+    private static Timer timer;
+    private static ILogger appLogger;
+    private static readonly ConcurrentDictionary<int, EventItem> EventList = new ConcurrentDictionary<int, EventItem>();
+    private static string LastContent;
+
+    public static void Initialize(ILogger logger)
+    {
+      appLogger = logger;
+    }
+
+    public static void AddEvent(string moduleName, int errorCode, string description, DateTime timestamp)
+    {
+      var eventItem = new EventItem
+      {
+        Module = moduleName,
+        ErrorCode = errorCode,
+        Message = description,
+        LocalTimestamp = timestamp,
+      };
+
+      EventList.AddOrUpdate(
+        errorCode,
+        eventItem,
+        (code, existingItem) =>
+          {
+            existingItem.LocalTimestamp = eventItem.LocalTimestamp;
+            return existingItem;
+          }
+        );
+
+
+      if (appLogger != null)
+      {
+        appLogger.LogDebug($"Added event to the queue: {eventItem}.");
+      }
+
+      if (timer == null)
+      {
+        // the 1st run will be in 10 seconds later, then
+        // run every 60 seconds
+        timer = new Timer(WriteLogFile, null, 0, 5000);
+      }
+    }
+
+    private static string GetLogFileWithFullPath()
+    {
+      var logDirectory = @"C:\temp";
+      return Path.Combine(logDirectory, LogFilename);
+    }
+
+    private static void WriteLogFile(object state)
+    {
+      try
+      {
+        var content = JsonConvert.SerializeObject(EventList);
+        if(content.Equals(LastContent, StringComparison.OrdinalIgnoreCase))
+        {
+          // file content does not change, so do not need to write
+          return;
+        }
+
+        File.WriteAllText(LogFileFullPath, content);
+        LastContent = content;
+      }
+      catch (Exception exception)
+      {
+        if (appLogger != null)
+        {
+          appLogger.LogError(exception, $"Failed to write events to file {LogFileFullPath}.");
+        }
+      }
+    }
+
+    private class EventItem
+    {
+      public string Module { get; set; }
+      public int ErrorCode { get; set; }
+      public string Message { get; set; }
+      public DateTime LocalTimestamp { get; set; }
+    }
+  }
+}
